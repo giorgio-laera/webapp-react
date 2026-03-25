e20b615b890c91141657aeec1df6e1d0ca1f908d
giorgio laera <laeragiorg@gmail.com>
2026-03-25 20:28:29 +0100

    aggiunta di map con card

--- src/Pages/MoviesPage.jsx
+++ src/Pages/MoviesPage.jsx
@@ -1,23 +1,34 @@
-import { Link } from "react-router-dom";
-import MoviesCard from "../component/movieCard"
+
+import MovieCard from "../component/movieCard"
 import style from "../modules/Movies.module.css"
 function moviesPage() {
+  const fakemovie = {
+    id: '1',
+    image: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg",
+    title : ' titolo',
+    relase_year : 'anno',
+    genre:'genere',
+    director:'direttore'
+  };
+  const moviese =[
+    fakemovie,
+    fakemovie
+  ]
+  console.log(moviese)
+  console.log(fakemovie)
   return (
     <div className={style.movies}>
       <h1>MoviesPage</h1>
-      <MoviesCard/>
-      <Link to="/">Go reviews</Link>
+<div className="d-flex movieCard-Container">
+  
+      {moviese.map(film=> < MovieCard movie={film} />)}
+</div>
+
+      
     </div>
   );
 }
 
 export default moviesPage;
 
-{/* <div>
-        image
-        titolo
-        anno 
-        genere
-        direttore
 
-      </div> */}
--- src/component/movieCard.jsx
+++ src/component/movieCard.jsx
@@ -1,15 +1,20 @@
+
+import { Link } from "react-router-dom";
 import style from "../modules/movieCard.module.css"
-function MoviesCard() {
+function MovieCard({movie}) {
+    const {id, image, title, relase_year, genre, director } = movie;
+    console.log("questo e id",movie)
     return (
-        <div className={style.card}>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg" alt="" />
-            <h2>titolo</h2>
-            <span>xx-xx-xxxx</span>
-            <p>genere</p>
-            <p>direttore</p>
+        <div  className={style.card} key={id}>
+            <img src={image} alt="" />
+            <h2>{title}</h2>
+            <span>{relase_year}</span>
+            <p>{genre}</p>
+            <p>{director}</p>
+            <Link to ={`/movies/:${id}`}>vai alle recenzioni</Link>
         </div>
     )
 }
-export default MoviesCard;
+export default MovieCard;
 
 
